3f27101c93227711ddb29dc6963c5a732115897f
Vic Cherubini <vcherubini@gmail.com>
2025-08-29 09:38:04 -0500

    wip

--- tests/PrettyPrintTest.php
+++ tests/PrettyPrintTest.php
@@ -2,10 +2,27 @@
 
 namespace OneToMany\PrettyPrint\Tests;
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\TestCase;
+
+use function OneToMany\PrettyPrint\pretty_print;
 
 #[Group('UnitTests')]
 final class PrettyPrintTest extends TestCase
 {
+    #[DataProvider('providerValueAndPrettyValue')]
+    public function testPrettyPrint(mixed $value, string $prettyValue): void
+    {
+        $this->assertSame(pretty_print($value), $prettyValue);
+    }
+
+    public static function providerValueAndPrettyValue(): array
+    {
+        $provider = [
+            [null, 'null'],
+        ];
+
+        return $provider;
+    }
 }
